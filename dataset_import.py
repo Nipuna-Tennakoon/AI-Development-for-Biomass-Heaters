85b87e66499051c6ea5366b84118043b77ded506
Nipuna-Tennakoon <tennakoon@medirai.com>
2025-11-05 19:33:21 +0530

    imported the dataset and created environment python==3.12

---
+++ dataset_import.py
@@ -0,0 +1,8 @@
+import gdown
+
+url = "https://drive.google.com/drive/folders/1U1zNGjZHCsfEYujS-oman3sseiuBtzRq?usp=sharing"
+id = "1U1zNGjZHCsfEYujS-oman3sseiuBtzRq?usp=sharing"
+gdown.download_folder(url)
+print("Dataset downloaded successfully.")
+
+
